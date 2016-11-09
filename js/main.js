375a361e0e3123c4893829b334db56f3ea76591d
Mike Eng <mrengy@gmail.com>
2016-11-08 22:58:47 -0500

    fading in silhouettes

--- js/main.js
+++ js/main.js
@@ -1,17 +1,31 @@
 $( document ).ready(function() {
-    var initialTimeout = window.setTimeout(fadeOutSlide1, 4);
+    var pauseTime = 4000;
+    var transitionTime = 3000;
+
+    var initialTimeout = window.setTimeout(fadeOutSlide1, pauseTime);
 
     function fadeOutSlide1(){
     	fadeOut($("#slide-1"));
+    	var afterSlide1Timeout = window.setTimeout(revealSlide3, pauseTime);
+    }
+
+    function revealSlide3(){
+    	reveal($("#slide-3"));
+    	console.log('revealSlide3 called');
     }
 
     function fadeOut(target){
-    	target.fadeTo(3,0,'swing',
+    	target.fadeTo(transitionTime,0,'swing',
     		function(){
     			target.addClass('hidden');
     		}
     	);
     }
 
+    function reveal(target){
+    	console.log('reveal called');
+    	//target.removeClass('hidden');
+    	target.fadeTo(transitionTime,1,'swing');
+    }
 
 });
