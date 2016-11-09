4c2b000ae473e0c1e46db50299b1a576f9c638a0
Mike Eng <mrengy@gmail.com>
2016-11-08 22:21:27 -0500

    centering image

--- js/main.js
+++ js/main.js
@@ -1,12 +1,12 @@
 $( document ).ready(function() {
-    var initialTimeout = window.setTimeout(fadeOutSlide1, 4000);
+    var initialTimeout = window.setTimeout(fadeOutSlide1, 4);
 
     function fadeOutSlide1(){
     	fadeOut($("#slide-1"));
     }
 
     function fadeOut(target){
-    	target.fadeTo(3000,0,'swing',
+    	target.fadeTo(3,0,'swing',
     		function(){
     			target.addClass('hidden');
     		}
